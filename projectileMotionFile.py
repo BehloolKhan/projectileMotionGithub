8af77622aca88d4903a43f613a0ea9812048b41e
Behlool_Khan <behloolk2020@gmail.com>
2026-01-21 17:31:59 +0000

    pyame window pops up, displaying projectile motion in action

---
+++ projectileMotionFile.py
@@ -0,0 +1,112 @@
+#import all required libraries
+import pygame #for rendering and display
+import numpy #for physics/maths
+import matplotlib #for graphs
+
+class Ball(pygame.sprite.Sprite):
+
+    acc = 9.81
+
+    def __init__(self, surfaceOfBall: pygame.Surface, colorOfBall:tuple, center:pygame.Vector2, radius):
+
+        #invoke constructor method of parent class
+
+        super().__init__()
+        self.color = colorOfBall
+        self.radius = radius
+        self.center = center
+
+        self.surface = surfaceOfBall
+        self.speed_h = 20
+        self.speed_v = -20
+
+        self.scale_factor = 10
+
+        
+        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+
+
+    def update(self, time):
+        #update the co-ordinates
+        self.center.x += time*self.speed_h*self.scale_factor
+        self.center.y += self.speed_v*time*self.scale_factor
+        
+        #update the velocity:
+        #horizontal remains the same
+        #vertical changes
+        self.speed_v = (self.speed_v) + Ball.acc*time
+
+    def draw(self, surfaceOfBall):
+
+        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+
+        #we are going to draw a line
+        #what will happen is that the starting point = center
+
+        verticalVelocityVector = self.center + pygame.Vector2(0, self.speed_v*self.scale_factor)
+        pygame.draw.line(self.surface, (255, 255, 255), self.center, verticalVelocityVector, 5)
+
+        horizontalVelocityVector = self.center + pygame.Vector2(self.speed_h*self.scale_factor, 0)
+        pygame.draw.line(self.surface, (0, 255, 0), self.center, horizontalVelocityVector, 5)
+
+        velocityVector = self.center + pygame.Vector2(self.speed_h*self.scale_factor, self.speed_v*self.scale_factor)
+        pygame.draw.line(self.surface, (255, 0, 0), self.center, velocityVector, 5)
+
+        #draw the acceleration aswell
+
+        accelerationVector = self.center + (pygame.Vector2(0, Ball.acc)) #scale the vector by a half
+        pygame.draw.line(self.surface, (255, 0, 255), self.center, accelerationVector, 5)
+
+
+#set up the dimensions of screen and everything
+
+def mainGameLoop():
+
+    pygame.init()
+
+    bg_colour = (0, 0, 0) # Black
+    BLUE_COLOUR = (0, 0, 255) # blue
+
+    SCREEN_WIDTH = 1400
+    SCREEN_HEIGHT = 800
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(bg_colour) #specify the color over here
+
+    ball_radius = 10
+    ball_centre = pygame.Vector2(20.0, SCREEN_HEIGHT/2)
+
+    ball = Ball(screen, BLUE_COLOUR, ball_centre, ball_radius) #draw the circle
+    Clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+
+        screen.fill(bg_colour) #it will get wiped out by this
+
+        for event in pygame.event.get():
+
+            if (event.type == pygame.QUIT):
+
+                running = False
+
+            elif (event.type == pygame.KEYDOWN):
+
+                if (event.key == pygame.K_ESCAPE):
+
+                    running = False
+
+        
+        ball.draw(screen) #draw the ball
+
+        pygame.display.flip() #update the display
+
+        change_time = Clock.tick(500) #lets see what happens when u increase it
+        change_time = change_time / 500 #convert it to seconds
+        ball.update(change_time)
+    
+    pygame.quit() #show down the pyagme window from here
+
+if __name__ == "__main__":
+    mainGameLoop()
