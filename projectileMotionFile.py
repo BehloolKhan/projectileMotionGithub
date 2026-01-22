2ad5d172d37a9a304a52c16bb3f450afbf0ed292
Behlool_Khan <behloolk2020@gmail.com>
2026-01-22 19:11:53 +0000

    included air resistance now aswell

--- projectileMotionFile.py
+++ projectileMotionFile.py
@@ -8,26 +8,31 @@ class Ball(pygame.sprite.Sprite):
 
     #down = positive, due to pygame co-ordinate system
 
-    acc = -9.81
+    _mass = 10 #mass 10 KG
+    _accelerationDueToWeight = 9.81
 
-    def __init__(self, surfaceOfBall: pygame.Surface, colorOfBall:tuple, center:pygame.Vector2, radius, initialVelocity:int, angle:int):
+    def __init__(self, surfaceOfBall: pygame.Surface, colorOfBall:tuple, center:pygame.Vector2, radius, initialVelocity:int, angle:int, k=0):
 
         #invoke constructor method of parent class
 
         super().__init__()
+
         self.color = colorOfBall
         self.radius = radius
         self.center = center
         self.surface = surfaceOfBall
+        self.K = k #by defualt, this value is 0
+        self.verticalAcceleration = 9.81 #starting acceleration
+        self.horizontalAcceleration = 0 #starting acceleration
 
         #need to convert angle value into radians
         angle = ((numpy.pi) / 180) * angle
 
-        self.speed_horizontal = initialVelocity*math.cos(angle)
+        self.speed_horizontal = -initialVelocity*math.cos(angle)
         self.speed_vertical = initialVelocity*math.sin(angle)
         self.scale_factor = 10 #10 pixels = 1 meter
 
-        self.line_scale_factor = 4
+        self.line_scale_factor = 4 #4 pixels = one meter
         
         pygame.draw.circle(self.surface, self.color, self.center, self.radius)
 
@@ -35,12 +40,25 @@ class Ball(pygame.sprite.Sprite):
     def update(self, time):
         #update the co-ordinates
         self.center.x += time*self.speed_horizontal*self.scale_factor
-        self.center.y += -self.speed_vertical*time*self.scale_factor
+        self.center.y += self.speed_vertical*time*self.scale_factor
         
         #update the velocity:
-        #horizontal remains the same
-        #vertical changes
-        self.speed_vertical = (self.speed_vertical) + Ball.acc*time
+        #need access to k value
+        #need access to current velocity
+        #F = -KV
+
+        resistiveHorizontal = -self.K*self.speed_horizontal
+        resistiveVertical = -self.K*self.speed_vertical
+
+        #take into account weight now aswell
+        resistiveVertical += (Ball._accelerationDueToWeight*Ball._mass)
+
+        #now we need to update the respective vertical and horizontal components of speed
+        self.horizontalAcceleration = resistiveHorizontal/Ball._mass
+        self.verticalAcceleration = resistiveVertical/Ball._mass
+
+        self.speed_horizontal += (self.horizontalAcceleration*time)
+        self.speed_vertical += (self.verticalAcceleration*time) # That is now done
 
     def draw(self, surfaceOfBall):
 
@@ -49,18 +67,18 @@ class Ball(pygame.sprite.Sprite):
         #we are going to draw a line
         #what will happen is that the starting point = center
 
-        verticalVelocityVector = self.center + pygame.Vector2(0, -self.speed_vertical*self.line_scale_factor)
+        verticalVelocityVector = self.center + pygame.Vector2(0, self.speed_vertical*self.line_scale_factor)
         pygame.draw.line(self.surface, (255, 255, 255), self.center, verticalVelocityVector, 5)
 
         horizontalVelocityVector = self.center + pygame.Vector2(self.speed_horizontal*self.line_scale_factor, 0)
         pygame.draw.line(self.surface, (0, 255, 0), self.center, horizontalVelocityVector, 5)
 
-        velocityVector = self.center + pygame.Vector2(self.speed_horizontal*self.line_scale_factor, -self.speed_vertical*self.line_scale_factor)
+        velocityVector = self.center + pygame.Vector2(self.speed_horizontal*self.line_scale_factor, self.speed_vertical*self.line_scale_factor)
         pygame.draw.line(self.surface, (255, 0, 0), self.center, velocityVector, 5)
 
         #draw the acceleration aswell
 
-        accelerationVector = self.center + (pygame.Vector2(0, -Ball.acc)) #scale the vector by a half
+        accelerationVector = self.center + (pygame.Vector2(self.line_scale_factor*self.horizontalAcceleration, self.line_scale_factor*self.verticalAcceleration)) #scale the vector by a half
         pygame.draw.line(self.surface, (255, 0, 255), self.center, accelerationVector, 5)
 
 
@@ -71,7 +89,7 @@ def displayProperties(destination: pygame.Surface, text:str, font:pygame.font.Fo
     ballProperites = font.render(text, True, textColor, (255, 255, 255)) # returns a surface, so need to blit on to another surface
     destination.blit(ballProperites, (topLeftX, topLeftY))
 
-def mainGameLoop(initialVelocity, angle):
+def mainGameLoop(initialVelocity, angle, k=0):
 
     pygame.init()
 
@@ -87,7 +105,7 @@ def mainGameLoop(initialVelocity, angle):
     ball_radius = 20
     ball_centre = pygame.Vector2(20.0, SCREEN_HEIGHT/2)
 
-    ball = Ball(screen, BLUE_COLOUR, ball_centre, ball_radius, initialVelocity, angle) #draw the circle
+    ball = Ball(screen, BLUE_COLOUR, ball_centre, ball_radius, initialVelocity, angle, k) #draw the circle
     Clock = pygame.time.Clock()
 
     cumulativeTime = 0 #keeps track of total seconds passed so far in seconds
@@ -115,7 +133,7 @@ def mainGameLoop(initialVelocity, angle):
         font = pygame.font.SysFont("Ariel", 30)
         textProperties = f"horizontal velocity: {ball.speed_horizontal} "
         displayProperties(screen, textProperties, font, (0, 0, 0), 0, SCREEN_HEIGHT-100)
-        textProperties = f"vertical velocity: {ball.speed_vertical}"
+        textProperties = f"vertical velocity: {-ball.speed_vertical}"
         displayProperties(screen, textProperties, font, (0, 0, 0), 0, SCREEN_HEIGHT-70)
         textProperties = f"time: {cumulativeTime} seconds"
         displayProperties(screen, textProperties, font, (0, 0, 0), 0, SCREEN_HEIGHT-40)
@@ -131,4 +149,4 @@ def mainGameLoop(initialVelocity, angle):
     pygame.quit() #show down the pyagme window from here
 
 if __name__ == "__main__":
-    mainGameLoop(30, 30)
+    mainGameLoop(-30, 30, 4)
